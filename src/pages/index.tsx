76028032893122c3a8f53e3f02c49292f5da3c6a
Sho0226 <s1F102201616@iniad.org>
2024-05-25 17:11:35 +0900

    css:holdとnextのflex指定

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,9 +4,13 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <div className={styles.tetriscontainer}>
-        <div className={styles.holdstyle} />
+        <div className={styles.holdcontainer}>
+          <div className={styles.holdstyle} />
+        </div>
         <div className={styles.boardstyle} />
-        <div className={styles.nextstyle} />
+        <div className={styles.nextcontainer}>
+          <div className={styles.nextstyle} />
+        </div>
       </div>
     </div>
   );
