aba9649e1eaf6de3f8660d1cac0f03e563958978
Sho0226 <s1F102201616@iniad.org>
2024-05-25 17:35:40 +0900

    css:board完成

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,13 @@ const Home = () => {
     <div className={styles.container}>
       <div className={styles.tetriscontainer}>
         <div className={styles.holdcontainer}>
+          <span className={styles.text}>HOLD</span>
+
           <div className={styles.holdstyle} />
         </div>
         <div className={styles.boardstyle} />
         <div className={styles.nextcontainer}>
+          <span className={styles.text}> NEXT</span>
           <div className={styles.nextstyle} />
         </div>
       </div>
