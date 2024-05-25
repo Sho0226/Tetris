eed6fd11257c959cb71b62ff7c980ef664f5b14b
Sho0226 <s1F102201616@iniad.org>
2024-05-25 16:57:59 +0900

    css:holdとnextと本体の枠組み

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,15 @@
 import styles from './index.module.css';
 
 const Home = () => {
-  return <div className={styles.container} />;
+  return (
+    <div className={styles.container}>
+      <div className={styles.tetriscontainer}>
+        <div className={styles.holdstyle} />
+        <div className={styles.boardstyle} />
+        <div className={styles.nextstyle} />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
